fa40e98ba46c2cae94582f379ba7762b8a9292d9
Eligos <2284226884@qq.com>
2026-03-11 18:41:46 +0800

    添加Styling的一些测试

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -4,8 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.darkMode}>
+        <Text style={styles.darkModeText}> !样式继承!
+          <Text style={styles.boldText}>
+            子文本会从父文本继承样式
+          </Text>
+        </Text>
+      </View>
+      <Text style={styles.title}>Start Working on Your App!</Text>
+      <View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
+        <Text>这是一个蓝色盒子</Text>
+      </View>
+      <View style={[styles.box, styles.lightgreenBg, styles.androidBoxShadow]}>
+        <Text>这是一个绿色盒子</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +29,55 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title:
+  {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "green",
+  },
+  darkMode:
+  {
+    backgroundColor: "black",
+  },
+  darkModeText:
+  {
+    color: "white",
+  },
+  boldText:
+  {
+    fontWeight: "bold",
+  },
+  box:
+  {
+    width: 200,
+    height: 100,
+    // alignItems: "center",
+    // justifyContent: "center",
+    // padding: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "red",
+    borderStyle: "dashed",
+    borderRadius: 10,
+  },
+  lightblueBg:
+  {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg:
+  {
+    backgroundColor: "lightgreen",
+  },
+  boxShadow: {
+    shadowColor: "red",
+    shadowOffset: { width: 6, height: 6 },
+    shadowOpacity: 0.6,
+    shadowRadius: 2,
+  },
+  androidBoxShadow: {
+    elevation: 10,
   },
 });
